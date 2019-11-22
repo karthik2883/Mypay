3c37ee7beb8943a887e59b6e7b71ae2da442c6fb
karthik2883 <karthik_2883@rediffmail.com>
2019-11-22 15:46:46 +0530

    basic ui designed is ready

--- mypay-payment/index.js
+++ mypay-payment/index.js
@@ -1,15 +1,33 @@
 var express = require("express");
+var mqtt = require('mqtt');
+var client = mqtt.connect('mqtt://localhost:1883');
 var app = express();
 var router = express.Router();
 var path = __dirname + '/views/'; // this folder should contain your html files.
 
-router.get("/",function(req,res){
-  res.sendFile(path + "index.html");
+router.get("/", function (req, res) {
+    res.sendFile(path + "index.html");
 });
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/jq', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
-app.use("/",router);
+app.use("/", router);
 
-app.listen(3000,function(){
-  console.log("Live at Port 3000");
+app.listen(3000, function () {
+    console.log("Live at Port 3000");
 });
+/*
+
+client.on('connect', function () {
+    client.subscribe('presence', function (err) {
+        if (!err) {
+            client.publish('presence', 'Hello mqtt')
+        }
+    })
+})
+
+client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log(message.toString())
+    // client.end()
+})*/
--- mypay-protocol/index.js
+++ mypay-protocol/index.js
@@ -4,7 +4,10 @@ var client = mqtt.connect('mqtt://localhost:1883')
 client.on('connect', function () {
     client.subscribe('presence', function (err) {
         if (!err) {
-            client.publish('presence', 'Hello mqtt')
+            client.publish('presence', '2N8sA8PXGufmc19ZbRxYzVeoGsmYmnVBzsD')
+            client.publish('presence', '2N8sA8PXGufmc19ZbRxYzVeoGsmYmnVBzsD')
+            client.publish('presence', '2N8sA8PXGufmc19ZbRxYzVeoGsmYmnVBzsD')
+            client.publish('presence', '2N8sA8PXGufmc19ZbRxYzVeoGsmYmnVBzsD')
         }
     })
 })
