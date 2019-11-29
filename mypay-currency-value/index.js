9d828e762bef588f8b15f0382ce8f19fb2d049ba
karthik2883 <karthik_2883@rediffmail.com>
2019-11-29 15:43:32 +0530

    currency

--- mypay-currency-value/index.js
+++ mypay-currency-value/index.js
@@ -1,5 +1,5 @@
-  var Api = require('bittrex-api-node');
-
+var Api = require('bittrex-api-node');
+ // currency manual feed daily basis
 const api = Api({
     publicKey: '416cb75f62b1439097c2bc1f41919398',
     secretKey: 'ce97197c38c24132971f55ee56f091b3',
@@ -13,16 +13,21 @@ ws.on('disconnected', () => { /* ... */ });
 ws.connect((client) => {
     client.authenticate();
 
-    
     client.getSummaryDeltas((data) => {
-       //  console.log(data.Deltas.length);
+    
         var d = data.Deltas.filter(function (a) {
-           // console.log(a.MarketName);
+            
            return a.MarketName.match(/USD-/g);
             
         });
-        console.log(d.length);
-        console.log(d);
+       // console.log(d.length);
+       // console.log(d);
+        d.forEach(  element => {
+            
+           console.log("test")
+        });
+        
+       
         // if (data.Deltas[0]){
         //     console.log(data.Deltas[0].MarketName, data.Deltas[0].Last);
         // }
